aad3934792b974a55cc7b921c43a1e073144d5f3
Mayank-1709 <negim3@student.op.ac.nz>
2023-08-31 10:15:50 +1200

    Created a menu for the driver options to track the participant count using a switch case

--- tourism-management-system/Program.cs
+++ tourism-management-system/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            int userChoice;
+            LoadDatabase();
+            do
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Display Tour Information");
+                Console.WriteLine("2. Track Participant Count");
+                Console.WriteLine("3. Generate Stakeholder Report");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("");
+                Console.Write("Enter your choice: ");
+                userInput = Console.ReadLine();
+                while (!int.TryParse(userInput, out userChoice))
+                {
+                    Console.WriteLine("Invalid Selection. Please enter your choice again: ");
+                    userInput = Console.ReadLine();
+                }
+                switch (userChoice)
+                {
+                    case 1:
+                        DisplayTourInformation();
+                        break;
+                    case 2:
+                        TrackParticipantCount();
+                        break;
+                    case 3:
+                        GenerateStakeHolderReport();
+                        break;
+                    case 4:
+                        SaveDatabase();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Write("Invalid Selection. Please enter your choice again: ");
+                        break;
+                }
+            } while (userChoice != 4);
         }
     }
 }
