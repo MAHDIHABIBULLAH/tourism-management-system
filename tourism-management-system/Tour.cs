c1af147fd1d902eb9149b7a17895f03ccd2b1d13
Mayank-1709 <negim3@student.op.ac.nz>
2023-08-31 10:00:01 +1200

    Created a tour class with different fields and added a text file with the tour details

---
+++ tourism-management-system/Tour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tourism_management_system
+{
+    public class Tour
+    {
+        private string tourName;
+        private int participantCount;
+        private string region;
+        private string date;
+        private string time;
+
+        public string TourName { get => tourName; set => tourName = value; }
+        public int ParticipantCount { get => participantCount; set => participantCount = value; }
+        public string Region { get => region; set => region = value; }
+        public string Date { get => date; set => date = value; }
+        public string Time { get => time; set => time = value; }
+
+        public Tour(string tourName, int participantCount, string region, string date, string time)
+        {
+            this.tourName = tourName;
+            this.participantCount = participantCount;
+            this.region = region;
+            this.date = date;
+            this.time = time;
+        }
+
+
+    }
+}
