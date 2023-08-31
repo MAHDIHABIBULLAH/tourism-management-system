a0b4851ec35ff4009ecd695ccfef59f1e8361fca
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-08-31 17:29:23 +1200

    made a method to save update remove and show customer data

--- tourism-management-system/Program.cs
+++ tourism-management-system/Program.cs
@@ -2,9 +2,51 @@
 {
     internal class Program
     {
+        static Dictionary<string, string> data = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Tourism Management System");
+                Console.WriteLine("1. Save Data");
+                Console.WriteLine("2. Update Data");
+                Console.WriteLine("3. Remove Data");
+                Console.WriteLine("4. Show All Data");
+                Console.WriteLine("5. Exit");
+                Console.Write("Enter your choice: ");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            SaveData();
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+                            ShowAllData();
+                            break;
+                        case 5:
+
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
